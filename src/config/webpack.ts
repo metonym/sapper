51c294624a8f37c905e8004ae82c1dfdd00015dd
Eric Liu <ericyl.us@gmail.com>
2020-04-10 15:29:08 -0700

    remove service worker

--- src/config/webpack.ts
+++ src/config/webpack.ts
@@ -1,55 +1,55 @@
-import { dev, src, dest } from './env';
+import { dev, src, dest } from "./env";
 
 export default {
-	dev,
+  dev,
 
-	client: {
-		entry: () => {
-			return {
-				main: `${src}/client`
-			};
-		},
+  client: {
+    entry: () => {
+      return {
+        main: `${src}/client`,
+      };
+    },
 
-		output: () => {
-			return {
-				path: `${dest}/client`,
-				filename: '[hash]/[name].js',
-				chunkFilename: '[hash]/[name].[id].js',
-				publicPath: `client/`
-			};
-		}
-	},
+    output: () => {
+      return {
+        path: `${dest}/client`,
+        filename: "[hash]/[name].js",
+        chunkFilename: "[hash]/[name].[id].js",
+        publicPath: `client/`,
+      };
+    },
+  },
 
-	server: {
-		entry: () => {
-			return {
-				server: `${src}/server`
-			};
-		},
+  server: {
+    entry: () => {
+      return {
+        server: `${src}/server`,
+      };
+    },
 
-		output: () => {
-			return {
-				path: `${dest}/server`,
-				filename: '[name].js',
-				chunkFilename: '[hash]/[name].[id].js',
-				libraryTarget: 'commonjs2'
-			};
-		}
-	},
+    output: () => {
+      return {
+        path: `${dest}/server`,
+        filename: "[name].js",
+        chunkFilename: "[hash]/[name].[id].js",
+        libraryTarget: "commonjs2",
+      };
+    },
+  },
 
-	serviceworker: {
-		entry: () => {
-			return {
-				'service-worker': `${src}/service-worker`
-			};
-		},
+  serviceworker: {
+    entry: () => {
+      return {
+        "service-worker": `${src}/service-worker`,
+      };
+    },
 
-		output: () => {
-			return {
-				path: dest,
-				filename: '[name].js',
-				chunkFilename: '[name].[id].[hash].js'
-			}
-		}
-	}
+    output: () => {
+      return {
+        path: dest,
+        filename: "[name].js",
+        chunkFilename: "[name].[id].[hash].js",
+      };
+    },
+  } /* */,
 };
